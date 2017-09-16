56dae47cc4851a7840ae4fc60c5778730653b677
BBT <995836360@qq.com>
2017-09-16 13:29:22 +0800

    MPDemo

---
+++ MusicPlayerDemo/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace _01音乐播放器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnBefore = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSP = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(153, 292);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // btnBefore
+            // 
+            this.btnBefore.Location = new System.Drawing.Point(175, 92);
+            this.btnBefore.Name = "btnBefore";
+            this.btnBefore.Size = new System.Drawing.Size(75, 23);
+            this.btnBefore.TabIndex = 1;
+            this.btnBefore.Text = "上一曲";
+            this.btnBefore.UseVisualStyleBackColor = true;
+            this.btnBefore.Click += new System.EventHandler(this.btnBefore_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(175, 209);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "下一曲";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(175, 34);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "打开音乐";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSP
+            // 
+            this.btnSP.Location = new System.Drawing.Point(175, 145);
+            this.btnSP.Name = "btnSP";
+            this.btnSP.Size = new System.Drawing.Size(75, 23);
+            this.btnSP.TabIndex = 4;
+            this.btnSP.Text = "暂停";
+            this.btnSP.UseVisualStyleBackColor = true;
+            this.btnSP.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 329);
+            this.Controls.Add(this.btnSP);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnBefore);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "晕音乐";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnBefore;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSP;
+    }
+}
+
---
+++ MusicPlayerDemo/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Media;
+namespace _01音乐播放器
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<String> list = new List<string>();
+        /// <summary>
+        /// 添加音乐
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "请选择要打开音乐文件";
+            ofd.Multiselect = true;
+            ofd.InitialDirectory = @"C:\Users\99583\Desktop\Music";
+            ofd.Filter = "WAV格式|*.wav|所有格式|*.*";
+            ofd.ShowDialog();
+            //将选中的音乐文件名添加到listBox中
+            string[] path = ofd.FileNames;
+            if (path==null)
+            {
+                return;
+            }
+            for (int i = 0; i < path.Length; i++)
+            {
+                listBox1.Items.Add(Path.GetFileName(path[i]));
+                list.Add(path[i]);
+            }
+            listBox1.SelectedIndex = 0;
+        }
+        SoundPlayer sp = new SoundPlayer();
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            sp.SoundLocation = list[listBox1.SelectedIndex];
+            sp.Play();
+        }
+
+        private void btnBefore_Click(object sender, EventArgs e)
+        {
+            if ((listBox1.SelectedIndex < 0) || (listBox1.SelectedIndex >= list.Count))
+            {
+                MessageBox.Show("你还没有添加歌曲呢！");
+                return;
+            }
+            int index = listBox1.SelectedIndex;
+             index--;
+            if ((index == 0)||(listBox1.SelectedIndex == 0))
+            {
+                index = listBox1.Items.Count-1;
+               
+            }
+            listBox1.SelectedIndex = index;
+            sp.SoundLocation = list[index];
+            sp.Play();
+            btnSP.Text = "暂停";
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if ((listBox1.SelectedIndex < 0) || (listBox1.SelectedIndex >= list.Count))
+            {
+                MessageBox.Show("你还没有添加歌曲呢！");
+                return;
+            }
+            int index = listBox1.SelectedIndex;
+            index++;
+            if ((index == listBox1.Items.Count) || (listBox1.SelectedIndex == listBox1.Items.Count))
+            {
+                index = 0;
+               
+            }
+            listBox1.SelectedIndex = index;
+            sp.SoundLocation = list[index];
+            sp.Play();
+            btnSP.Text = "暂停";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((listBox1.SelectedIndex < 0) || (listBox1.SelectedIndex >= list.Count))
+            {
+                MessageBox.Show("你还没有添加歌曲呢！");
+                return;
+            }
+            if (btnSP.Text == "暂停")
+            {
+                sp.Stop();
+            btnSP.Text = "播放";
+            }
+            else if (btnSP.Text == "播放")
+            {
+                sp.Play();
+                btnSP.Text ="暂停";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("欢迎进入汪医晕音乐");
+        }
+    }
+}
